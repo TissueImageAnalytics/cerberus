fa7cace2f38bd1d8fbf4e9b0e033de1c15921120
Simon Graham <simon.graham@warwick.ac.uk>
2022-11-08 06:13:34 +0000

    UPD: Save thumbnail as default

--- run_wsi.sh
+++ run_wsi.sh
@@ -1,5 +1,7 @@
 python run_infer_wsi.py \
 --gpu="0" \
+--batch_size=25 \ # dependent on hardware
 --model="/root/romesco_workspace/resnet34_cerberus" \
 --input_dir="wsi_test/" \
 --output_dir="output_test/"
+--save_thumb
