486a5dcc538ec9d223806da059e9b2ffafbb1ad7
Simon Graham <simon.graham@warwick.ac.uk>
2022-11-16 13:05:51 +0000

    NEW: Add run shell script

---
+++ run.sh
@@ -0,0 +1,10 @@
+python run.py \
+--gpu="0" \
+--fold=1 \
+--nuclei_inst \
+--nuclei_dir="dataset/patches/nuclei" \
+--gland_inst \
+--gland_dir="dataset/patches/gland" \
+--lumen_inst \
+--lumen_dir="dataset/patches/lumen" \
+--mix_target_in_batch
